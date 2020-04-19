150cd1e9137fe502600f945a098b1d28e3845e73
zoomi-raja <zoom_karyala@yahoo.com>
2020-04-19 19:44:24 +0400

    routes mounting

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+// routes
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
-app.get('/', (req, res) => {
-  res.status(200).json({ msg: 'hello from server side' });
-});
-app.post('/', (req, res) => {
-  res.status(200).json({ msg: 'post call received' });
-});
+app.use(express.json());
+app.use(morgan('dev'));
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port num ${port} ....`);
