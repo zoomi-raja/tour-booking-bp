7d881a59e51f0a026822724c53049c4a30b1ca06
zoomi-raja <zoom_karyala@yahoo.com>
2020-04-24 16:49:05 +0400

    tour stats api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -98,3 +98,31 @@ exports.deleteTour = async (req, res) => {
     return res.status(404).send({ status: 'fail', message: err });
   }
 };
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+    res.status(200).json({ status: 'success', data: stats });
+  } catch (err) {
+    res.status(404).json({
+      stats: 'fail',
+      message: err,
+    });
+  }
+};
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,5 +1,6 @@
 class APIFeatures {
   constructor(query, queryString) {
+    this.orignalObj = query;
     this.query = query;
     this.queryString = queryString;
   }
@@ -34,14 +35,14 @@ class APIFeatures {
     return this;
   }
 
-  paginate() {
+  async paginate() {
     // page=2&limit=10
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
     this.query = this.query.skip(skip).limit(limit);
     if (this.queryString.page) {
-      //   const newTours = await this.query.countDocuments();
+      // const newTours = await this.orignalObj.countDocuments();
       //   if (skip >= newTours) throw new Error('this page does not exists');
     }
     return this;
