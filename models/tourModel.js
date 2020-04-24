7524471beb1334fed6066600b8b97d62d230a759
zoomi-raja <zoom_karyala@yahoo.com>
2020-04-24 19:59:00 +0400

    model tour validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,43 +3,65 @@ const mongoose = require('mongoose');
 const tourSchema = mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have name'],
+    required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters'],
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
   },
   duration: {
     type: Number,
-    required: [true, 'A tour must have duration'],
+    required: [true, 'A tour must have a duration'],
   },
   maxGroupSize: {
     type: Number,
-    required: [true, ['a tour must have group size']],
+    required: [true, 'A tour must have a group size'],
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have difficulty'],
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult',
+    },
   },
   ratingsAverage: {
     type: Number,
     default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
   },
-  ratingsQuantity: { type: Number, default: 0 },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'A tour must have price'],
+    required: [true, 'A tour must have a price'],
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function (val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price',
+    },
+  },
   summary: {
-    type: String,
-    trim: true,
-  },
-  description: {
     type: String,
     trim: true,
     required: [true, 'A tour must have a description'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
   imageCover: {
     type: String,
-    required: [true, 'A tour must have cover image'],
+    required: [true, 'A tour must have a cover image'],
   },
   images: [String],
   createdAt: {
@@ -48,6 +70,10 @@ const tourSchema = mongoose.Schema({
     select: false,
   },
   startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
+  },
 });
 tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
