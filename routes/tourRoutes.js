e58144869246a47e5bce568fdb70590fbf3fbf31
zoomi-raja <zoom_karyala@yahoo.com>
2020-04-24 18:25:04 +0400

    tour report by year api

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,7 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopCheapTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
   .get(tourController.getAllTours)
