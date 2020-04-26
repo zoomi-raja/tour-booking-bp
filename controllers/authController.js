95de5d4a8b73c54fb252543074d2218f250153ad
zoomi-raja <zoom_karyala@yahoo.com>
2020-04-26 11:31:35 +0400

    fix for signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,11 @@ const catchAsync = (fn) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUseer = await User.create(req.body);
+  const newUseer = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
   res.status(201).json({ status: 'success', data: { user: newUseer } });
 });
