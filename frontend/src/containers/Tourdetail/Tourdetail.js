74ea7ef394e03b609909981201d952be21626f1b
zoomi-raja <zoomi.raja@gmail.com>
2020-05-09 13:45:34 +0400

    guide div

--- frontend/src/containers/Tourdetail/Tourdetail.js
+++ frontend/src/containers/Tourdetail/Tourdetail.js
@@ -6,6 +6,7 @@ import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
 import image1 from '../../assets/qudra-image-1.jpeg';
 import image2 from '../../assets/qudra-image-2.jpg';
 import image3 from '../../assets/qudra-image-3.jpg';
+import tourGuide from './zoomi.jpeg';
 mapboxgl.accessToken =
   'pk.eyJ1Ijoiem9vbWktcmFqYSIsImEiOiJjazl6NDhvMDcwdXN2M3RwazlzbzF4OWFiIn0.1n67PHfJehyEBhaym3zScQ';
 class Tourdetail extends React.Component {
@@ -125,6 +126,82 @@ class Tourdetail extends React.Component {
               <img src={image1} alt="tour place 1" />
               <img src={image2} alt="tour place 2" />
               <img src={image3} alt="tour place 3" />
+            </div>
+          </div>
+          <div className={classes['key-details']}>
+            <div className={classes.guides__section}>
+              <h2>Your tour guides</h2>
+              <div className={classes.guides}>
+                <figure className={classes.guides__shape}>
+                  <img
+                    src={tourGuide}
+                    alt="shape"
+                    className={classes.guides__image}
+                  />
+                  <figcaption className={classes.guides__caption}>
+                    <span>lead Guide</span>zoomi
+                  </figcaption>
+                </figure>
+                <figure className={classes.guides__shape}>
+                  <img
+                    src={tourGuide}
+                    alt="shape"
+                    className={classes.guides__image}
+                  />
+                  <figcaption className={classes.guides__caption}>
+                    <span>Guide</span>zoomi
+                  </figcaption>
+                </figure>
+                <figure className={classes.guides__shape}>
+                  <img
+                    src={tourGuide}
+                    alt="shape"
+                    className={classes.guides__image}
+                  />
+                  <figcaption className={classes.guides__caption}>
+                    <span>Guide</span>zamurd ali
+                  </figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className={classes['detail-box']}>
+              <h2>Key Details</h2>
+              <ul>
+                <li className={classes['detail-box__detail']}>
+                  <svg className={classes['detail-box__icon']}>
+                    <use xlinkHref={`${svgSprite}#icon-calendar`}></use>
+                  </svg>
+                  <span className={classes['detail-box__label']}>
+                    Next date
+                  </span>
+                  <span className={classes['detail-box__text']}>June 2021</span>
+                </li>
+                <li className={classes['detail-box__detail']}>
+                  <svg className={classes['detail-box__icon']}>
+                    <use xlinkHref={`${svgSprite}#icon-flag`}></use>
+                  </svg>
+                  <span className={classes['detail-box__label']}>
+                    DIFFICULTY
+                  </span>
+                  <span className={classes['detail-box__text']}>medium</span>
+                </li>
+                <li className={classes['detail-box__detail']}>
+                  <svg className={classes['detail-box__icon']}>
+                    <use xlinkHref={`${svgSprite}#icon-user`}></use>
+                  </svg>
+                  <span className={classes['detail-box__label']}>
+                    participants
+                  </span>
+                  <span className={classes['detail-box__text']}>15 People</span>
+                </li>
+                <li className={classes['detail-box__detail']}>
+                  <svg className={classes['detail-box__icon']}>
+                    <use xlinkHref={`${svgSprite}#icon-star`}></use>
+                  </svg>
+                  <span className={classes['detail-box__label']}>rating</span>
+                  <span className={classes['detail-box__text']}>4.8/5</span>
+                </li>
+              </ul>
             </div>
           </div>
           <div
