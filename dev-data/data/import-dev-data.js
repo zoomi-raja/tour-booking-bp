861d8281b3e8b3d9043de5a5defe0b46814e055b
zoomi-raja <zoomi.raja@gmail.com>
2020-05-29 12:03:41 +0400

    get tour near to user in home screen

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -26,8 +26,8 @@ const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`));
 const importData = async () => {
   try {
     await Tour.create(tours);
-    // await User.create(users, { validateBeforeSave: false });
-    // await Review.create(reviews);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('data successfully loaded');
     process.exit();
   } catch (err) {
@@ -38,8 +38,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    // await Review.deleteMany();
-    // await User.deleteMany();
+    await Review.deleteMany();
+    await User.deleteMany();
     console.log('data has been successfully deleted');
     process.exit();
   } catch (err) {
---
+++ frontend/src/containers/CarouselContainer/CarouselContainer.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import classes from './Carousel.module.scss';
+import Carousel from '../../components/Carousel/Carousel';
+
+import axios from '../../utils/Axios';
+import Aux from '../../hoc/Aux';
+
+class CarouselContainer extends React.Component {
+  state = {
+    loading: true,
+    carousel: [],
+    range: 5,
+    lat: 25.066006,
+    lng: 55.137757,
+  };
+  getCoordinates() {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
+  async fetchData(lat, lng, range) {
+    const geoApiURL = `/tours/tours-within/${range}/center/${lat},${lng}/unit/km/`;
+    const results = await axios.get(geoApiURL);
+    if (results.data.data.tour.length > 0) {
+      const data = results.data.data.tour;
+      this.setState({
+        ...this.state,
+        carousel: data,
+        loading: false,
+        lat,
+        lng,
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        loading: false,
+      });
+    }
+  }
+  async componentDidMount() {
+    if ('geolocation' in navigator) {
+      const position = await this.getCoordinates();
+      await this.fetchData(
+        position.coords.latitude,
+        position.coords.longitude,
+        this.state.range
+      );
+      if (this.state.carousel.length === 0) {
+        this.fetchData(this.state.lat, this.state.lng, this.state.range);
+      }
+    }
+    this.setState({ ...this.state, loading: false });
+  }
+  render() {
+    let html;
+    if (this.state.carousel.length > 0) {
+      html = (
+        <Aux>
+          <h1>
+            Locations near You...!
+            <select
+              value={this.state.range}
+              onChange={(e) => {
+                const value = parseInt(e.target.value);
+                if (Number.isInteger(value)) {
+                  this.setState({ ...this.state, range: value });
+                  this.fetchData(this.state.lat, this.state.lng, value);
+                }
+              }}
+            >
+              <option value="5">5 KM</option>
+              <option value="10">10 KM</option>
+              <option value="20">20 KM</option>
+              <option value="30">30 KM</option>
+              <option value="40">40 KM</option>
+              <option value="50">50 KM</option>
+            </select>
+          </h1>
+          <Carousel items={this.state.carousel} />
+        </Aux>
+      );
+    }
+    if (this.state.loading) {
+      html = (
+        <div className={classes.loader}>
+          <div className={classes.loader__item}></div>
+          <div className={classes.loader__item}></div>
+          <div className={classes.loader__item}></div>
+        </div>
+      );
+    }
+    return <div className={classes.carousal_wrapper}>{html}</div>;
+  }
+}
+export default CarouselContainer;
